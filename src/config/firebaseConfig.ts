96c158fc6218f654269705c6249f73e6dde869d7
Ravdeep Sharma <rsharma1.academic@rrc.ca>
2025-10-24 01:35:01 -0500

    Configure Firebase Admin SDK and initialize authentication setup.

---
+++ src/config/firebaseConfig.ts
@@ -0,0 +1,10 @@
+import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
+import { getAuth, Auth } from "firebase-admin/auth";
+
+import serviceAccount from "src/firebase-service-account.json";
+
+initializeApp({
+  credential: cert(serviceAccount as ServiceAccount),
+});
+
+export const auth: Auth = getAuth();
